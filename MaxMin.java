b963ed3861ce60568d97d65c4b285fac4b98393f
Karoliny <131821537+karolinyyap@users.noreply.github.com>
2025-08-21 13:32:02 -0300

    Add files via upload

---
+++ MaxMin.java
@@ -0,0 +1,16 @@
+
+package maxmin;
+
+public class MaxMin {
+
+    public static void main(String[] args) {
+        int a = 20, b = 30;
+        
+        double c = -5.9, d = -4.5;
+        
+        //Exemplo de sobrecarga
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.max(c,d));
+    }
+    
+}
---
+++ MetodoFloor.java
@@ -0,0 +1,37 @@
+
+package metodofloor;
+
+public class MetodoFloor {
+
+    
+    public static void main(String[] args) {
+        double a = 6.2, b = 6.6, c = -6.8;
+        
+        //Floor
+        /*
+        System.out.println(Math.floor(a));
+        System.out.println(Math.floor(b));
+        System.out.println(Math.floor(c));
+        */
+        
+        //Ceil
+        /*
+        System.out.println(Math.ceil(a));
+        System.out.println(Math.ceil(b));
+        System.out.println(Math.ceil(c));
+        */
+        
+        //Teste da aula
+        /*
+        double nota = 6.9;
+        int minhaNota1, minhaNota2;
+        
+        minhaNota1 = (int) Math.ceil(Math.floor(nota));
+        minhaNota2 = (int) Math.floor(Math.ceil(nota));
+        
+        System.out.println(minhaNota1);
+        System.out.println(minhaNota2);
+        */
+    }
+    
+}
